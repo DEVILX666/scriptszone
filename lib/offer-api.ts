a9aa957e659fd5c4c80ad868d2823dc2975afab6
DEVILX666 <batatabatatax5@gmail.com>
2025-11-01 19:10:52 +0100

    Update offer-api.ts

--- lib/offer-api.ts
+++ lib/offer-api.ts
@@ -25,7 +25,7 @@ export async function fetchOffers(userIP: string, userAgent: string, maxOffers =
       user_agent: userAgent,
       max: "6",
       min: "6",
-      ctype: "5",
+      ctype: "1",
     })
 
     const response = await fetch(`${API_BASE_URL}?${params}`, {
